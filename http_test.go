18c8092647926bc657e73e1ac53f8a716512c405
Anders Kalhauge <anders@kalhauge.dk>
2016-08-19 14:33:08 +0200

    Roman Five test

--- http_test.go
+++ http_test.go
@@ -85,4 +85,17 @@ func TestRomanFour(t *T) {
         t.Fatalf("wrong body returned: %s", body)
     }
 
+func TestRomanFive(t *T) {
+    n := romanGenerator(1)
+    r, _ := http.NewRequest("GET", "/roman/5", nil)
+    w := httptest.NewRecorder()
+    n.ServeHTTP(w, r)
+    if w.Code != 200 {
+        t.Fatalf("wrong code returned: %d", w.Code)
+    }
+    body := w.Body.String()
+    if body != fmt.Sprintf("Here's your number: V\n") {
+        t.Fatalf("wrong body returned: %s", body)
+    }
+
 }
