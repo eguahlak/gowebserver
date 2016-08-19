134c458227316868a15ab10336193d5d6dbbf772
Anders Kalhauge <anders@kalhauge.dk>
2016-08-19 16:40:15 +0200

    PTI3

--- jobDSL/Utils.groovy
+++ jobDSL/Utils.groovy
@@ -5,8 +5,9 @@ static def configureGit(def job, def repositoryUrl) {
                 remote {
                     name('origin')
                     url(repositoryUrl)
+					credentials('1d5b1f18-2a96-4fab-9a9d-3597ee4b3241')
                 }
-                branch('ready/**')
+                branch('*/ready/**')
             }
         }
         triggers {
